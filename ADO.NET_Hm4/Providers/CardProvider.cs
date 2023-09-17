9129f4547de42776c6998cc9bf44330111dd51d9
Refer3n <refer3n@gmail.com>
2023-09-18 01:57:42 +0300

    Initial Commit

---
+++ ADO.NET_Hm4/Providers/CardProvider.cs
@@ -0,0 +1,56 @@
+﻿using Pract4.DAL.Entities;
+using Pract4.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract4
+{
+    public class CardProvider
+    {
+        private readonly IRepository<StudentCard> _repository;
+
+        public CardProvider(IRepository<StudentCard> repository)
+        {
+            _repository = repository;
+        }
+
+        public void AddCards(List<StudentCard> students)
+        {
+            students.ForEach(student => AddCard(student));
+        }
+
+        public void AddCard(StudentCard card)
+        {
+            _repository.Add(card);
+        }
+
+        public void RemoveCards(List<StudentCard> cards)
+        {
+            cards.ForEach(card => RemoveStudent(card));
+        }
+
+        public void RemoveStudent(StudentCard card)
+        {
+            _repository.Remove(card);
+        }
+
+        public void RemoveCard(int Id)
+        {
+            var card = GetCard(Id);
+            _repository.Remove(card);
+        }
+
+        public StudentCard GetCard(int Id)
+        {
+            return _repository.Get(Id);
+        }
+
+        public IEnumerable<StudentCard> GetCards()
+        {
+            return _repository.GetAll();
+        }
+    }
+}
---
+++ ADO.NET_Hm4/Providers/StudentProvider.cs
@@ -0,0 +1,52 @@
+﻿using Pract4.DAL.Entities;
+using Pract4.DAL.Repositories;
+using System.Collections.Generic;
+
+namespace Pract4
+{
+    public class StudentsProvider
+    {
+        private readonly IRepository<Student> _repository;
+
+        public StudentsProvider(IRepository<Student> repository)
+        {
+            _repository = repository;
+        }
+
+        public void AddStudents(List<Student> students)
+        {
+            students.ForEach(student => AddStudent(student));
+        }
+
+        public void AddStudent(Student student)
+        {
+            _repository.Add(student);
+        }
+
+        public void RemoveStudents(List<Student> students)
+        {
+            students.ForEach(student => RemoveStudent(student));
+        }
+
+        public void RemoveStudent(Student student)
+        {
+            _repository.Remove(student);
+        }
+
+        public void RemoveStudent(int Id)
+        {
+            var student = GetStudent(Id);
+            _repository.Remove(student);
+        }
+
+        public Student GetStudent(int Id)
+        {
+            return _repository.Get(Id);
+        }
+
+        public IEnumerable<Student> GetStudents()
+        {
+            return _repository.GetAll();
+        }
+    }
+}
